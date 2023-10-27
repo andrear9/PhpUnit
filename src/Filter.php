866f9005684c7d63619ca6ec85c4fd01426b9483
andrear9 <fonzibubu@gmail.com>
2023-10-27 14:47:45 +0200

    test con phpunit

---
+++ src/Filter.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+class Filter
+{
+    public function isEmail(string $email): bool
+    {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return true;
+        }
+        return false;
+    }
+}
